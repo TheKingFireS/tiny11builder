6dcd8f9eac5e0dc06c61a1f707e48840adcdff22
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-04-21 23:44:15 +0200

    Create get-oscdimg.bat

---
+++ get-oscdimg.bat
@@ -0,0 +1,20 @@
+pushd %~dp0
+
+curl.exe https://download.microsoft.com/download/6/7/4/674ec7db-7c89-4f2b-8363-689055c2b430/adk/Installers/52be7e8e9164388a9e6c24d01f6f1625.cab --remote-name
+curl.exe https://download.microsoft.com/download/6/7/4/674ec7db-7c89-4f2b-8363-689055c2b430/adk/Installers/5d984200acbde182fd99cbfbe9bad133.cab --remote-name
+curl.exe https://download.microsoft.com/download/6/7/4/674ec7db-7c89-4f2b-8363-689055c2b430/adk/Installers/9d2b092478d6cca70d5ac957368c00ba.cab --remote-name
+curl.exe https://download.microsoft.com/download/6/7/4/674ec7db-7c89-4f2b-8363-689055c2b430/adk/Installers/bbf55224a0290f00676ddc410f004498.cab --remote-name
+curl.exe https://download.microsoft.com/download/6/7/4/674ec7db-7c89-4f2b-8363-689055c2b430/adk/Installers/Oscdimg%%20(DesktopEditions)-x86_en-us.msi --output "Oscdimg (DesktopEditions)-x86_en-us.msi"
+
+mkdir temp
+msiexec.exe /a "Oscdimg (DesktopEditions)-x86_en-us.msi" /qn TARGETDIR=%cd%\temp
+copy "temp\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe" "oscdimg.exe"
+rmdir /s /q temp
+
+del 52be7e8e9164388a9e6c24d01f6f1625.cab
+del 5d984200acbde182fd99cbfbe9bad133.cab
+del 9d2b092478d6cca70d5ac957368c00ba.cab
+del bbf55224a0290f00676ddc410f004498.cab
+del "Oscdimg (DesktopEditions)-x86_en-us.msi"
+
+popd
